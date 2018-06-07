b06e992da2d84494810a5b0bbb90cb9bf60dd284
vincemasciale <vince@lonesuspect.com>
2018-06-06 21:48:59 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value.username);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+}
